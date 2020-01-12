1b8c09e05b3e9b9aef1e706c0d95c63e3d8c9c82
SedanIV <58529875+SedanIV@users.noreply.github.com>
2020-01-12 12:12:41 +0300

    Add files via upload

---
+++ Assets/scripts/Play.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public void BAMSPressed()
+    {
+        SceneManager.LoadScene("building_obj");
+    }
+
+    public void CSPressed()
+    {
+        SceneManager.LoadScene("building_obj");
+    }
+
+    public void JavaPressed()
+    {
+        SceneManager.LoadScene("building_obj");
+    }
+}
