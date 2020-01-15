99bba5c2830197e5eccf63c2684f6416f059c2d4
regiri <goparegiri@gmail.com>
2020-01-15 10:14:33 +0300

    another merge

---
+++ Assets/scripts/UIScripts/progressCheckScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class progressCheckScript : MonoBehaviour
+{
+    public Sprite[] sprites = new Sprite[5];
+    public GameObject obj;
+    IEnumerator Times() {
+        obj.GetComponent<SpriteRenderer>().sprite = sprites[0];
+        yield return new WaitForSeconds(.1f);
+        obj.GetComponent<SpriteRenderer>().sprite = sprites[1];
+        yield return new WaitForSeconds(.1f);
+        obj.GetComponent<SpriteRenderer>().sprite = sprites[2];
+        yield return new WaitForSeconds(.1f);
+        obj.GetComponent<SpriteRenderer>().sprite = sprites[3];
+        yield return new WaitForSeconds(.1f);
+        obj.GetComponent<SpriteRenderer>().sprite = sprites[4];
+    }
+    void Start() {
+        StartCoroutine("Times");
+    }
+}
