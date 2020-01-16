fc590c5b3f205026cbc5f5af9d321cee8f1cf183
regiri <goparegiri@gmail.com>
2020-01-16 10:11:04 +0300

    merge

--- Assets/scripts/EscapeMenuOpen.cs
+++ Assets/scripts/EscapeMenuOpen.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -29,35 +28,3 @@ public class EscapeMenuOpen : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class EscapeMenuOpen : MonoBehaviour
-{
-    public GameObject GameMenuCanvas;
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            GameMenuCanvas.SetActive(true);
-            Time.timeScale = 0;
-        }
-        
-    }
-
-    public void SetMenuOff()
-    {
-        GameMenuCanvas.SetActive(false);
-        Time.timeScale = 1;
-    }
-
-    public void CloseGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-    }
-}
->>>>>>> 3eca2d77c50576be206f84248e78083dc6c2f363
